9f84f07d0fdba523de7e6bfb960184de580b1985
sunilkumar <sunilkumar@sunils-mbp>
2021-06-21 13:01:42 +0530

    assessment

---
+++ src/main/java/com/alpha/exam/exam/model/Product.java
@@ -0,0 +1,29 @@
+package com.alpha.exam.exam.model;
+
+public class Product {
+	
+	private double 	price;
+	private String continent;
+	private int multiplier;
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getContinent() {
+		return continent;
+	}
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+	public int getMultiplier() {
+		return multiplier;
+	}
+	public void setMultiplier(int multiplier) {
+		this.multiplier = multiplier;
+	}
+	
+	
+
+}
